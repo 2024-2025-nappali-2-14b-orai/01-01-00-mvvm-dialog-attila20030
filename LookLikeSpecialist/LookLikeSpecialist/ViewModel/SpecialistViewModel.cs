0168a745f4a2b1ecbf09a5f4a4ea03d9fcbd542e
Attila <szt3daratt@vasvari.org>
2024-11-28 17:39:11 +0100

    fel

--- LookLikeSpecialist/LookLikeSpecialist/ViewModel/SpecialistViewModel.cs
+++ LookLikeSpecialist/LookLikeSpecialist/ViewModel/SpecialistViewModel.cs
@@ -1,13 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using LookLikeSpecialist.Models;
+using System.Collections.ObjectModel;
 
-namespace LookLikeSpecialist.ViewModel
+namespace LookLikeSpecialist.ViewModels
 {
-    public class SpecialistViewModel
+    public partial class SpecialistViewModel : ObservableObject
     {
 
+        [ObservableProperty]
+        private Specialist _specialist = new Specialist();
+
+
+
+        [ObservableProperty]
+        private ObservableCollection<Specialist> _specialists = new ObservableCollection<Specialist>();
+
+        [ObservableProperty]
+        private Specialist _selectedSpecialist = new Specialist();
+
+        public SpecialistViewModel()
+        {
+            Specialist.Szuldatum = DateTime.Now.AddYears(-14);
+        }
+
+        [RelayCommand]
+        public void DoSave(Specialist specialist)
+        {
+            Specialists.Add(specialist);
+            Specialist = new Specialist();
+            Specialist.Szuldatum = DateTime.Now.AddYears(-14);
+            OnPropertyChanged(nameof(Specialist));
+        }
+
+        [RelayCommand]
+        public void DoNewSpecialist()
+        {
+            Specialist = new Specialist();
+            Specialist.Szuldatum = DateTime.Now.AddYears(-14);
+            OnPropertyChanged(nameof(Specialist));
+        }
+
+        [RelayCommand]
+        public void DoDelete(Specialist specialist)
+        {
+            Specialists.Remove(Specialist);
+            Specialist = new Specialist();
+            Specialist.Szuldatum = DateTime.Now.AddYears(-14);
+            OnPropertyChanged(nameof(Specialist));
+        }
     }
 }
